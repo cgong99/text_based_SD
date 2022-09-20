70ca981184dd8b3db7d48d105ad1264734d16f81
cgong99 <gongcchen999@gmail.com>
2022-09-20 19:19:48 -0400

    cut all audio

--- cut_audio.py
+++ cut_audio.py
@@ -1,17 +1,18 @@
+import os
 from pydub import AudioSegment
 
-def cut_audio(audio, transcript):
-  start, end = find_start_end_time(transcript)
+def cut_audio(export_path, audio_path, trancript_path, audio, transcript):
+  start, end = find_start_end_time(trancript_path+transcript)
   print(start/1000, end/1000)
-  sound = AudioSegment.from_mp3(audio)
+  sound = AudioSegment.from_mp3(audio_path+ audio)
   print(type(sound))
   extract = sound[start:end]
   
   audio_name = audio[:audio.find(".")]
-  export_name = audio_name + "_cut.mp3"
+  export_name = export_path + audio_name + "_cut.mp3"
   print(export_name)
   extract.export(export_name, format="mp3")
-  pass
+  
 
 def find_start_end_time(transcript_name):
   start_time, end_time = None, None
@@ -35,12 +36,25 @@ def find_start_end_time(transcript_name):
   
   return start_time, end_time
       
-      
+def match_file_without_extension(file1, file2):
+  name1 = file1[:file1.find(".")]
+  name2 = file2[:file2.find(".")]
+  return name1==name2
 
 if __name__ == "__main__":
-  audio = "CallHome_eval/4093.mp3"
-  file = "CallHome_eval/4093.cha"
-  cut_audio(audio, file)
+  audio_path = "CallHome_eval/raw_audio/"
+  transcript_path = "CallHome_eval/transcripts/"
+  export_path = "CallHome_eval/cut_audio/"
+  
+  all_audio = os.listdir(audio_path)
+  all_transcripts = os.listdir(transcript_path)
+  
+  for audio_file in all_audio:
+    for transcript_file in all_transcripts:
+      if match_file_without_extension(audio_file, transcript_file):
+        cut_audio(export_path, audio_path, transcript_path, audio_file, transcript_file)
+    
+  # cut_audio(audio, file)
 
 
         
