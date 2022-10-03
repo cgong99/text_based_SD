d6357c806a4f270fbf79bfc4a7a95c553e496f63
cgong99 <gongcchen999@gmail.com>
2022-10-03 17:57:58 -0400

    levensthein distance

--- alignment/sequence-alignment.py
+++ alignment/sequence-alignment.py
@@ -1,8 +1,38 @@
+import numpy as np
 
 
 
+def LevenshteinDistance(s, t):
+  m, n = len(s), len(t)
+  d = np.zeros((m, n))
 
+  for i in range(1,m):
+    d[i, 0] = i
+ 
+  for j in range(1,n):
+    d[0, j] = j
+ 
+  for j in range(1,n):
+    for i in range(1,m):
+      if s[i] == t[j]:
+        substitutionCost = 0
+      else:
+        substitutionCost = 1
+        
+      d[i, j] = min(d[i-1, j] + 1, d[i, j-1] + 1, d[i-1, j-1] + substitutionCost) 
+  return d[m-1, n-1]
 
+class NW:
+  def __init__(self):
+    pass
+  
+  def backtrack(self):
+    pass
+  
+  def compute_table(self):
+    pass
 
 if __name__ == "__main__":
-  pass
+  s = "fuck"
+  t = "Hello"
+  print(LevenshteinDistance(s,t))
