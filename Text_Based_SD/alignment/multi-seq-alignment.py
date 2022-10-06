f6c7431c156af5be15f057e1846a981a194fa03c
Peilin Wu <peilinwu1217@gmail.com>
2022-10-06 18:16:03 -0400

    Update: reformat the code following python offical PEP8 style guid

--- Text_Based_SD/alignment/multi-seq-alignment.py
+++ Text_Based_SD/alignment/multi-seq-alignment.py
@@ -6,106 +6,109 @@ from typing import List, Tuple
 from sklearn.metrics import get_scorer
 from NeedlemanWunsch import edit_distance, get_scoring_matrix
 
+
 def score(token1, token2, token3) -> int:
-  """return compare score for three tokens
+    """return compare score for three tokens
 
-  Args:
-      token1 (string): 
-      token2 (string): 
-      token3 (string): 
+    Args:
+        token1 (string):
+        token2 (string):
+        token3 (string):
 
-  Returns:
-      int: score
-  """
-  pass
+    Returns:
+        int: score
+    """
+    pass
+
 
 class MultiSeqAlign:
-  def __init__(self, target, seq1, seq2) -> None:
-    """initialize a 3-d matrix's 3 surfaces, and stores the three sequence
-        target: m*1 x dimiension
-        seq1: n*1 y dimiension
-        seq2: d*1 z dimiension
-    Args:
-        seq1 (List): 
-        seq2 (List): 
-        seq3 (List): 
-    """
-    self.gap = -1
-    self.target, self.seq1, self.seq2 = target, seq1, seq2
-    self.m, self.n, self.d = len(target)+1, len(seq1)+1, len(seq2)+1
-    self.matrix = np.zeros((self.m, self.n, self.d))
-    self.setupMatrix()
-  
-  def setupMatrix(self):
-    """init 3 edges with gap values, and compute 3 surface using pairwise alignment
-    """
-    for i in range(self.m):
-      self.matrix[i,0,0] = self.gap * i
-    for i in range(self.n):
-      self.matrix[0,i,0] = self.gap * i
-    for i in range(self.d):
-      self.matrix[0,0,i] = self.gap * i
-    # setup xy
-    self.setupSurface("xy")
-    # setup yz
-    self.setupSurface("yz")
-    # setup xz
-    self.setupSurface("xz")
-    
-  def setupSurface(self, axes: String):
-    """setup one surface of a matrix based on the passed in axes, computing a 2d sequence alignment table
-    Args:
-        axes: "xy", "yz" or "xz"
-    """
-    x,y,z = 1,1,1
-    table = None
-    if axes == 'xy':
-      x = self.m
-      y = self.n
-      table = get_scoring_matrix(seq1=self.target, seq2=self.seq1, gap=self.gap)
-    elif axes == 'yz':
-      y = self.n
-      z = self.d
-      table = get_scoring_matrix(seq1=self.seq1, seq2=self.seq2, gap=self.gap)
-    elif axes == 'xz':
-      x = self.m
-      z = self.d
-      table = get_scoring_matrix(seq1=self.target, seq2=self.seq2, gap=self.gap)
-    else:
-      print("dimension error: ", axes, " should be xy yz or xz")
-    
-    # the unselected dimension only contain 0
-    for i in range(x):
-      for j in range(y):
-        for k in range(z):
-          if axes == 'xy':
-            self.matrix[i,j,k] = table[i,j]
-          elif axes == 'yz':
-            self.matrix[i,j,k] = table[j,k]
-          elif axes == 'xz':
-            self.matrix[i,j,k] = table[i,k]
-            
-  def compute_matrix(self):
-    for i in range(self.m):
-      for j in range(self.n):
-        for k in range(self.d):
-          #self.matrix[i,j,k] = 
-          xi = self.target[i]
-          yj = self.seq1[j]
-          zk = self.seq2[k]
-          xGap = score('-',yj,zk) + self.matrix[i,j-1,k-1]
-          yGap = score(xi,'-',zk) + self.matrix[i-1,j,k-1]
-          zGap = score(xi,yj,'-') + self.matrix[i-1,j-1,k]
-          yzGap = score(xi,'-','-') + self.matrix[i-1,j,k]
-          xzGap = score('-',yj,'-') + self.matrix[i,j-1,k]
-          xyGap = score('-','-',zk) + self.matrix[i,j,k-1]
-          allMatch = score(xi,yj,zk) + self.matrix[i-1,j-1,k-1]
-          self.matrix[i,j,k] = max([xGap, yGap, zGap, yzGap, xyGap, xzGap, allMatch])
-            
+    def __init__(self, target, seq1, seq2) -> None:
+        """initialize a 3-d matrix's 3 surfaces, and stores the three sequence
+            target: m*1 x dimiension
+            seq1: n*1 y dimiension
+            seq2: d*1 z dimiension
+        Args:
+            seq1 (List):
+            seq2 (List):
+            seq3 (List):
+        """
+        self.gap = -1
+        self.target, self.seq1, self.seq2 = target, seq1, seq2
+        self.m, self.n, self.d = len(target) + 1, len(seq1) + 1, len(seq2) + 1
+        self.matrix = np.zeros((self.m, self.n, self.d))
+        self.setupMatrix()
+
+    def setupMatrix(self):
+        """init 3 edges with gap values, and compute 3 surface using pairwise alignment
+        """
+        for i in range(self.m):
+            self.matrix[i, 0, 0] = self.gap * i
+        for i in range(self.n):
+            self.matrix[0, i, 0] = self.gap * i
+        for i in range(self.d):
+            self.matrix[0, 0, i] = self.gap * i
+        # setup xy
+        self.setupSurface("xy")
+        # setup yz
+        self.setupSurface("yz")
+        # setup xz
+        self.setupSurface("xz")
+
+    def setupSurface(self, axes: String):
+        """setup one surface of a matrix based on the passed in axes, computing a 2d sequence alignment table
+        Args:
+            axes: "xy", "yz" or "xz"
+        """
+        x, y, z = 1, 1, 1
+        table = None
+        if axes == 'xy':
+            x = self.m
+            y = self.n
+            table = get_scoring_matrix(seq1=self.target, seq2=self.seq1, gap=self.gap)
+        elif axes == 'yz':
+            y = self.n
+            z = self.d
+            table = get_scoring_matrix(seq1=self.seq1, seq2=self.seq2, gap=self.gap)
+        elif axes == 'xz':
+            x = self.m
+            z = self.d
+            table = get_scoring_matrix(seq1=self.target, seq2=self.seq2, gap=self.gap)
+        else:
+            print("dimension error: ", axes, " should be xy yz or xz")
+
+        # the unselected dimension only contain 0
+        for i in range(x):
+            for j in range(y):
+                for k in range(z):
+                    if axes == 'xy':
+                        self.matrix[i, j, k] = table[i, j]
+                    elif axes == 'yz':
+                        self.matrix[i, j, k] = table[j, k]
+                    elif axes == 'xz':
+                        self.matrix[i, j, k] = table[i, k]
+
+    def compute_matrix(self):
+        for i in range(self.m):
+            for j in range(self.n):
+                for k in range(self.d):
+                    # self.matrix[i,j,k] =
+                    xi = self.target[i]
+                    yj = self.seq1[j]
+                    zk = self.seq2[k]
+                    xGap = score('-', yj, zk) + self.matrix[i, j - 1, k - 1]
+                    yGap = score(xi, '-', zk) + self.matrix[i - 1, j, k - 1]
+                    zGap = score(xi, yj, '-') + self.matrix[i - 1, j - 1, k]
+                    yzGap = score(xi, '-', '-') + self.matrix[i - 1, j, k]
+                    xzGap = score('-', yj, '-') + self.matrix[i, j - 1, k]
+                    xyGap = score('-', '-', zk) + self.matrix[i, j, k - 1]
+                    allMatch = score(xi, yj, zk) + self.matrix[i - 1, j - 1, k - 1]
+                    self.matrix[i, j, k] = max([xGap, yGap, zGap, yzGap, xyGap, xzGap, allMatch])
+
+
 if __name__ == "__main__":
-  target = "AGTTG"
-  seq1 = "AG"
-  seq2 = "GTT"
-  align = MultiSeqAlign(target, seq1, seq2)
-  print(align.matrix)
-  print(align.matrix.shape)
+    target = "AGTTG"
+    seq1 = "AG"
+    seq2 = "GTT"
+    align = MultiSeqAlign(target, seq1, seq2)
+    print(align.matrix)
+    print(align.matrix.shape)
