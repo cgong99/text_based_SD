4e2f6991e0e546352290ffcd737fbab213ea0712
cgong99 <gongcchen999@gmail.com>
2022-11-01 16:32:29 -0400

    4093 rev txt

--- Text_Based_SD/alignment/createTestData.py
+++ Text_Based_SD/alignment/createTestData.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import csv
 from Text_Based_SD.data.TranscriptProcess import CallHome
+from NeedlemanWunsch3D import *
 
 
 def readAlignment(name):
@@ -13,6 +14,18 @@ def readAlignment(name):
   output = lines[2].replace("\n", "").split(",")
   # print(spk2)
   return spk1, spk2, output
+
+def getTokenSeq(filename):
+  f = open(filename)
+  lines = f.readlines()
+  # spk1_tokens, spk2_tokens, output_tokens = [],[],[]
+  spk1 = lines[0].replace("\n", "").split(",")
+  spk2 = lines[1].replace("\n", "").split(",")
+  output = lines[2].replace("\n", "").split(",")
+  spk1_tokens = [token for token in spk1 if token != "-"]
+  spk2_tokens = [token for token in spk2 if token != "-"]
+  output_tokens = [token for token in output if token != "-"]
+  return spk1_tokens, spk2_tokens, output_tokens
 
 def extractAlignmentArray(name):
   spk1, spk2, output = readAlignment(name)
@@ -90,12 +103,13 @@ if __name__ == "__main__":
   # writeFile("4074_test.csv", spk1, spk2, output)
   
   spk1_gt, spk2_gt = extractAlignmentArray("4074_test.csv")
-  spk1, spk2 = extractAlignmentArray("4074_correctness_test_result.csv")
+  spk1, spk2 = extractAlignmentArray("4074_disable_spk12_align_result.csv")
   # spk1 = spk1[1:]
   # spk2 = spk2[1:]
   print(len(spk1_gt), len(spk1))
-  print(spk2[0:50])
-  print(spk2_gt[0:50])
+  print(len(spk2_gt), len(spk2))
+  print(spk1[150:200])
+  print(spk1_gt[150:200])
   count = 0
   for i in range(len(spk1_gt)):
     if spk1[i] != spk1_gt[i]:
@@ -106,6 +120,12 @@ if __name__ == "__main__":
   print("difference count: ", count)
   print("accuracy: ", ((len(spk1)+len(spk2))-count)/(len(spk1)+len(spk2)))
   
+  # seq1, seq2, target = getTokenSeq("4074_test.csv")
+  # align1, align2, align3, align2_to_align1, align3_to_align1 = backtrack(target, seq1, seq2, get_scoring_matrix_3d(target, seq1, seq2, 4074), 4074)
+                                                                                           
+  # with open(f"4074_disable_spk12_align_result.csv", 'w') as file:
+  #   output = csv.writer(file)
+  #   output.writerows([align2, align3, align1, align2_to_align1, align3_to_align1])
 
 
   
